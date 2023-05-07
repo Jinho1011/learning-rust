5ce9c99e2a11ff8f3108d6edbcd42581b1bd4748
Jeon <gino9940@gmail.com>
2023-05-08 01:51:54 +0900

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate rand;
+
+use std::io::stdin;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("{}", secret_number);
+
+    while let Ok(num) = guess.trim().parse::<i32>() {
+        if num < secret_number {
+            println!("higher");
+        } else if num > secret_number {
+            println!("lower");
+        } else {
+            println!("correct!");
+            break;
+        }
+        guess.clear();
+        stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    }
 }
