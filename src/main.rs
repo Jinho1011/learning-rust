6ba146488d6655b9894713b3b5ea69f8108d2070
Jeon <gino9940@gmail.com>
2023-05-08 15:14:02 +0900

    fix: replace if/else to match

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
 extern crate rand;
 
+use std::cmp::Ordering;
 use std::io::stdin;
 use rand::{Rng, thread_rng};
 
 fn main() {
-    let mut guess = String::new();
     let secret_number = thread_rng().gen_range(1..101);
 
-    stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    let mut num = guess.trim().parse::<i32>().unwrap();
-
-    println!("{}", secret_number);
-
-
     loop {
-        if num < secret_number {
-            println!("higher");
-        } else if num > secret_number {
-            println!("lower");
-        } else {
-            println!("correct!");
-            break;
-        }
-        guess.clear();
+        print!("input your guess: ");
+
+        let mut guess = String::new();
         stdin().read_line(&mut guess)
             .expect("Failed to read line");
-        num = guess.trim().parse().unwrap();
+        let guess: i32 = guess.trim().parse().unwrap();
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less"),
+            Ordering::Greater => println!("Greater"),
+            Ordering::Equal => {
+                println!("Equal");
+                break;
+            }
+        }
     }
 }
